4ada0aa73d3d205ce626d251c831a3521793b562
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2023-12-19 17:38:42 +0100

    made changes to package.json

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const dotenv = require("dotenv")
 
 dotenv.config();
 // Setting variables for the different views
-const publicDir = path.join(__dirname, "../public");
-const viewsDir = path.join(__dirname, "../templates/views");
-const partialsDir = path.join(__dirname + "../../templates/partials");
+const publicDir = path.join(__dirname, "./public");
+const viewsDir = path.join(__dirname, "./templates/views");
+const partialsDir = path.join(__dirname, "./templates/partials");
 
 // Setting up the templating engine => Register '.mustache' extension with The Mustache Express
 // Setting up the partials engine passed to mustacheExpress(....)
@@ -27,9 +27,13 @@ app.use(express.json());
 
 // custom middleware to verify the time of the request
 app.use((req, res, next) => {
-  const time = new Date().toUTCString();
-  req.time = time;
-  next();
+  const time = new Date().getHours();
+  if(time >= 9 && time <= 16) {
+    next()
+  }else {
+    res.status(200).json({message: "This webpage is only accessible between working hours (09:00 to 17:00)"})
+  }
+  
 })
 
 
